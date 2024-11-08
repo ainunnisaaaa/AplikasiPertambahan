0806c0a82eedb7093d5b44181097d5c0a05525c0
ainunnisaaaa <ainunnisa60268@gmail.com>
2024-11-08 21:47:31 +0800

    Menambahkan GUI components dan mendesain aplikasi

--- src/PertambahanDuaAngka.java
+++ src/PertambahanDuaAngka.java
@@ -32,11 +32,11 @@ public class PertambahanDuaAngka extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 517, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 359, Short.MAX_VALUE)
         );
 
         pack();
